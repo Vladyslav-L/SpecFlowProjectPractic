cb8046df162dcc3b7474f12b7d90a107a586f6e0
Vlad Liashenko <pactur@ukr.net>
2021-05-27 16:05:24 +0300

    fix

--- SpecFlowProjectPractic/Contains.cs
+++ SpecFlowProjectPractic/Contains.cs
@@ -3,5 +3,6 @@
     public static class Constants
     {
         public const string Password = "123qweQWE!";
+        public const string NewPassword = "123qweQWE!--";
     }
 }
--- SpecFlowProjectPractic/Steps/API/ChangePasswordSteps.cs
+++ SpecFlowProjectPractic/Steps/API/ChangePasswordSteps.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using NUnit.Framework;
+using SpecFlowProjectPractic.ApiRequests.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace SpecFlowProjectPractic.Steps.API
 {
+    [Binding]
     class ChangePasswordSteps
     {
+        private readonly ScenarioContext _scenarioContext;
+        private ClientAuthModel _user;       
+        private string _changedPassword;
+
+        public ChangePasswordSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            _user = _scenarioContext.Get<ClientAuthModel>(Context.User);
+        }
+
+        [When(@"I send the request POST to route /password/change/ with valid body and authorization token")]
+        public void ISendTheRequestPostToRourePasswordChangeWithValidBodyAndAuthorizationToken()
+        {
+            _changedPassword = ClientRequests.SendReguestChangeClientPasswordPost(Constants.Password, Constants.NewPassword, _user.TokenData.Token);
+        }
+
+        [Then(@"Client password is changed")]
+        public void ClientProfilePhotoIsUploaded()
+        {
+            Assert.AreEqual(_user.TokenData.Token, _changedPassword);
+        }
     }
 }
